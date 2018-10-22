5da717f27af988caf1c409aa6f5f25071d3dc052
Safarali <mr.safarali@gmail.com>
2018-10-22 16:25:59 -0700

    Set up the project

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NatoursApp from './components/NatoursApp';
+import './styles/styles.scss';
+
+ReactDOM.render(<NatoursApp/>, document.getElementById('app'));
---
+++ src/components/NatoursApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const NatoursApp = () => (
+    <h1>I should appear here now</h1>
+);
+
+export default NatoursApp;
