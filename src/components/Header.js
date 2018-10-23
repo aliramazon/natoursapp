6f773e3cc36572d7f0d00c1af96a510685c35719
Safarali <mr.safarali@gmail.com>
2018-10-23 01:24:18 -0700

    Create Header and style header image

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Header = () => (
+    <Fragment>
+        <header className="header">
+            I am from Header!!!
+        </header>
+    </Fragment>
+);
+
+export default Header;
