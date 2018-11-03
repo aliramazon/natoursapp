8c58532ede8272ff7d3c6be6e5a53985363c5220
Safarali <mr.safarali@gmail.com>
2018-11-03 16:46:29 -0700

    Switch to BEM

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,20 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import logoWhite from "../images/logo-white.png";
 import Button from './Button';
 
 const Header = () => (
-    <Fragment>
-        <header className="header">
-            <div className="logo-box">
-                <img className="logo" src={logoWhite} alt="Logo" />
-            </div>
-            <div className="text-box">
-                <h1 className="heading-primary">
-                    <span className="heading-primary-main">Outdoors</span>
-                    <span className="heading-primary-sub">is where life happens</span>
-                </h1>
-                <Button/>
-            </div>
-        </header>
-    </Fragment>
+    <header className="header">
+        <div className="header__logo-box">
+            <img className="header__logo" src={logoWhite} alt="Logo" />
+        </div>
+        <div className="header__text-box">
+            <h1 className="heading-primary">
+                <span className="heading-primary--main">Outdoors</span>
+                <span className="heading-primary--sub">is where life happens</span>
+            </h1>
+            <Button/>
+        </div>
+    </header>
 );
 
 export default Header;
