33a8d27c3b1ef80df24b6d5a7397cbaedea58199
Safarali <mr.safarali@gmail.com>
2018-11-06 00:22:42 -0800

    Create grid using floats

---
+++ src/components/Grid.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Grid = () => (
+    <section className="grid-test">
+        <div className="row">
+            <div className="col-1-of-2">
+                Col 1 of 2
+            </div>
+            <div className="col-1-of-2">
+                Col 1 of 2
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-1-of-3">
+                Col 1 of 3
+            </div>
+            <div className="col-1-of-3">
+                Col 1 of 3
+            </div>
+            <div className="col-1-of-3">
+                Col 1 of 3
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-1-of-3">
+                Col 1 of 3
+            </div>
+            <div className="col-2-of-3">
+                Col 2 of 3
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-1-of-4">
+                Col 1 of 4
+            </div>
+            <div className="col-1-of-4">
+                Col 1 of 4
+            </div>
+            <div className="col-1-of-4">
+                Col 1 of 4
+            </div>
+            <div className="col-1-of-4">
+                Col 1 of 4
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-1-of-4">
+                Col 1 of 4
+            </div>
+            <div className="col-1-of-4">
+                Col 1 of 4
+            </div>
+            <div className="col-2-of-4">
+                Col 2 of 4
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-1-of-4">
+                Col 1 of 4
+            </div>
+            <div className="col-3-of-4">
+                Col 3 of 4
+            </div>
+        </div>
+    </section>
+);
+
+// export default Grid;
